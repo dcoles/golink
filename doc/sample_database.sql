27d42081771850e8ef0b484d1157fbebb957e79f
David Coles <coles.david@gmail.com>
2018-06-26 14:41:46 +0000

    doc: Update sample database.
    Don't create tables if they already exists. Use INSERT OR REPLACE
    so Golinks can be overridden and add a link to the Github project.

--- doc/sample_database.sql
+++ doc/sample_database.sql
@@ -1,10 +1,11 @@
 -- Sample database
 -- Run `sqlite3 database '.read sample_database.sql'` to create.
 
-CREATE TABLE Golinks (name STRING PRIMARY KEY, url STRING);
+CREATE TABLE IF NOT EXISTS Golinks (name STRING PRIMARY KEY, url STRING);
 
-INSERT INTO Golinks (name, url) VALUES ('google', 'https://www.google.com');
-INSERT INTO Golinks (name, url) VALUES ('urlparse', 'https://docs.python.org/3/library/urllib.parse.html#url-parsing');
-INSERT INTO Golinks (name, url) VALUES ('drive', 'http://drive.google.com/');
-INSERT INTO Golinks (name, url) VALUES ('pylib', 'https://docs.python.org/3/library/');
-INSERT INTO Golinks (name, url) VALUES ('search', 'https://www.google.com/search?q=');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('golink', 'https://github.com/dcoles/golink');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('google', 'https://www.google.com');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('urlparse', 'https://docs.python.org/3/library/urllib.parse.html#url-parsing');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('drive', 'http://drive.google.com/');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('pylib', 'https://docs.python.org/3/library/');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('search', 'https://www.google.com/search?q=');
