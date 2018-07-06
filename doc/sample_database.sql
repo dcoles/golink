2194a209563fa3c11048b19a209469497bd9e1a1
David Coles <coles.david@gmail.com>
2018-07-06 08:27:25 -0700

    Update documentation.
    Server now has authentication, so remove the warning about not being
    suitable for use in untrusted environments.
    
    Explain what Golinks are and how they behave along with a demo hosted
    at http://go.dcoles.net.

--- doc/sample_database.sql
+++ doc/sample_database.sql
@@ -3,8 +3,10 @@
 
 CREATE TABLE IF NOT EXISTS Golinks (name VARCHAR PRIMARY KEY, url VARCHAR NOT NULL, owner VARCHAR);
 
-INSERT OR REPLACE INTO Golinks (name, url) VALUES ('golink', 'https://github.com/dcoles/golink');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('link', 'https://github.com/dcoles/golink/');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('github', 'https://github.com');
 INSERT OR REPLACE INTO Golinks (name, url) VALUES ('google', 'https://www.google.com');
+INSERT OR REPLACE INTO Golinks (name, url) VALUES ('example', 'http://example.com');
 INSERT OR REPLACE INTO Golinks (name, url) VALUES ('urlparse', 'https://docs.python.org/3/library/urllib.parse.html#url-parsing');
 INSERT OR REPLACE INTO Golinks (name, url) VALUES ('drive', 'http://drive.google.com/');
 INSERT OR REPLACE INTO Golinks (name, url) VALUES ('pylib', 'https://docs.python.org/3/library/');
